20f565d5975f3deec351d329ae110309e55f0c07
thakurkaran007 <thakurkaran0345@example.com>
2026-02-05 15:12:38 +0530

    final

---
+++ apps/frontend/app/layout.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@/auth";
+import "@repo/ui/src/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider session={session}>
+          {children}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
