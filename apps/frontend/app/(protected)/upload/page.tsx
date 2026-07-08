8e3d9cbd8298db75f4e46f93c0af52ab9cf88aa6
Karan Thakur <144684608+thakurkaran007@users.noreply.github.com>
2026-07-08 14:10:36 +0530

    Update page.tsx

--- apps/frontend/app/(protected)/upload/page.tsx
+++ apps/frontend/app/(protected)/upload/page.tsx
@@ -64,7 +64,7 @@ export default function UploadPage() {
           contentType: videoFile.type
       }
 
-      const res:any = await axios.post("https://backend.thakurkaran.com/api/v1/videos/upload", payload, { withCredentials: true });
+      const res:any = await axios.post("https://primary.thakurkaran.com/api/v1/videos/upload", payload, { withCredentials: true });
 
       if (res.status !== 200) {
         throw new Error(res.data.message || "Failed to get upload URL");
